f72a2adf63d26d90f6c11fa99c25111166cc4892
remi-1201 <remi.nkjm@gmail.com>
2022-06-01 18:44:23 +0900

    1/ Django Data Models

--- C2-Week1-2.sql
+++ C2-Week1-2.sql
@@ -1,3 +1,4 @@
+-- 1/ Django Data Models
 -- https://docs.google.com/presentation/d/15KWvjF2obSRwOSMO3ytvFfSTBhsx2DtT/ 
 
 -- Exact sqlite3
@@ -41,12 +42,30 @@ CREATE TABLE Users(
   name VARCHAR(128), 
   email VARCHAR(128)
 );
+
 -- ORM (models.py) - Slide no.9
 from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=128)
     email = models.CharField(max_length=128)
+
+-- CRUD in the ORM - Slide no.13
+sqlite3
+u = User(name='Sally', email='a2@umich.edu')
+u.save()
+
+User.objects.values()
+User.objects.filter(email='csev@umich.edu').values()
+
+User.objects.filter(email='ted@umich.edu').delete()
+User.objects.values()
+
+User.objects.filter(email='csev@umich.edu').update(name='Charles')
+User.objects.values()
+
+User.objects.values().order_by('email')
+User.objects.values().order_by('-name')
 
 -- Checking - Slide no.10
 sqlite3 db.sqlite3
@@ -56,6 +75,7 @@ sqlite3 db.sqlite3
 
 -- Inserting a Record - Slide no.11
 $ python3 manage.py shell
+
 -- SQL: INSERT INTO Users (name, email) VALUES ('Kristin', 'kf@umich.edu')
 from users.models import User
 u = User(name='Kristen', email='kf@umich.edu')
@@ -64,8 +84,21 @@ u.save()
 print(u.id) 
 print(u.email) 
 
-
 -- Checking SQL - Slide no.12
+sqlite3
 from django.db import connection
 print(connection.queries)
--- result [{'sql': 'INSERT INTO "users_user" ("name", "email") VALUES (\'Kristen\', \'kf@umich.edu\')', 'time': '0.052'}]
+-- result (debug data)
+-- [{'sql': 'INSERT INTO "users_user" ("name", "email") VALUES (\'Kristen\', \'kf@umich.edu\')', 'time': '0.052'}]
+
+-- makemigrations
+ls */models.py
+ls  */migrations/0*.py
+
+-- Re-running makemigrate
+rm bookone/migrations/0001_initial.py
+python3 manage.py makemigrations
+
+-- Re-running migrate from scratch
+rm db.sqlite3
+python3 manage.py migrate
